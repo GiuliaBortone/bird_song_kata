ce58cecba22567f669a04384982a604d6e3882de
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-02-13 10:55:15 +0100

    remove duplicate tests

--- test/TreeTest.java
+++ test/TreeTest.java
@@ -3,21 +3,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TreeTest {
-    @Test
-    void noBranchesAndBirdsCanGenerateZeroSongs() {
-        int birds = 0, branches = 0;
-        Tree tree = new Tree(branches);
-
-        assertEquals(0, tree.songsFor(birds));
-    }
-
-    @Test
-    void noBranchesAndOneBirdCanGenerateZeroSongs() {
-        int birds = 1, branches = 0;
-        Tree tree = new Tree(branches);
-
-        assertEquals(0, tree.songsFor(birds));
-    }
 
     @Test
     void noBranchesAndAnyBirdCanGenerateZeroSongs() {
