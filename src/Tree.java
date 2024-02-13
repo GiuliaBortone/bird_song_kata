e0f3d888c1005760de81606f8b786d3a4e2ac36b
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-02-13 10:28:20 +0100

    zero birds and branches produce zero songs

---
+++ src/Tree.java
@@ -0,0 +1,11 @@
+public class Tree {
+    private final int branches;
+
+    public Tree(int branches) {
+        this.branches = branches;
+    }
+
+    public int songsFor(int birds) {
+        return 0;
+    }
+}
---
+++ test/TreeTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TreeTest {
+    @Test
+    void noBranchesAndBirdsCanGenerateZeroSongs() {
+        int birds = 0, branches = 0;
+        Tree tree = new Tree(branches);
+
+        assertEquals(0, tree.songsFor(birds));
+    }
+}
